19d8fe0b99cd8ab203d3cca67713af32d3a5eec8
Chinmay Jindal <chinmayjindal1994@gmail.com>
2014-11-04 01:46:18 +0530

    triggers the exploration of url

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//String url = "https://put.io/v2/files/247219415/download?token=aac7236a800511e2852d001018321b64";
+		String url = "http://www.google.com";
+		Downloader d = new Downloader(url);
 	}
-
 }
