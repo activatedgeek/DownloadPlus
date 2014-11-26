00185d336fcb9a8740c9b51453ca98f17ac21b94
activatedgeek <ksanyam18@yahoo.com>
2014-11-26 21:03:10 +0530

    separated routines

--- Downloader.java
+++ Downloader.java
@@ -1,20 +1,7 @@
-import java.net.*;
 
 public class Downloader {
 	public Downloader(String url) {
-		new URIExplore(url);
+		URIExplore uri = new URIExplore(url);
+		Logger.debug(uri.finalURI);
 	}
-	
-	//String proxyInfo = "ee12b1021:123@192.168.36.22:3128"
-	public static Proxy setProxy(){
-		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.36.22", 3128));
-		Authenticator auth = new Authenticator(){
-			public PasswordAuthentication getPasswordAuthentication(){
-				return (new PasswordAuthentication("ee12b1021", "123".toCharArray()));
-			}
-		};
-		Authenticator.setDefault(auth);
-		return proxy;
-	}
-	
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 
 public class Main {
-
+	/* Enable/Disable Logging here */
+	static{
+		Logger.enableLog();
+		Logger.enableDebug();
+		//ConnectionProxy.setHTTPProxy("localhost", 8080);
+		ConnectionProxy.unsetHTTPProxy();
+	}
+	
 	public static void main(String[] args) {
 		//String url = "https://put.io/v2/files/247219415/download?token=aac7236a800511e2852d001018321b64";
-		String url = "http://www.google.com";
-		Downloader d = new Downloader(url);
+		new Downloader("http://www.le.co.in");
 	}
 }
