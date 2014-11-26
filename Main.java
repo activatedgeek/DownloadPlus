6be32317f0cb66b4dbe473fb313defe8605fceed
Chinmay Jindal <chinmayjindal1994@gmail.com>
2014-11-27 01:01:21 +0530

    Entry point misc changes

--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ public class Main {
 	static{
 		Logger.enableLog();
 		Logger.enableDebug();
-		//ConnectionProxy.setHTTPProxy("localhost", 8080);
-		ConnectionProxy.unsetHTTPProxy();
+		ConnectionProxy.setHTTPProxy("192.168.36.22", 3128, "ee12b1021", "123");
+		//ConnectionProxy.unsetHTTPProxy();
 	}
 	
 	public static void main(String[] args) {
-		//String url = "https://put.io/v2/files/247219415/download?token=aac7236a800511e2852d001018321b64";
-		new Downloader("http://www.le.co.in");
+		String url = "https://put.io/v2/files/253883579/download?token=aac7236a800511e2852d001018321b64";
+		//String url = "http://iitmweb.iitm.ac.in/phase2/courses/coursecontents/106108113/mod01lec01.mp4";
+		//String url = "https://www.facebook.com/";
+		Downloader d = new Downloader(url, "/home/chinmay/Desktop");
 	}
 }
