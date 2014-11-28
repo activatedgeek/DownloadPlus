d9c689c55f7fe8900526a4b0708c823e56db57a5
Chinmay Jindal <chinmayjindal1994@gmail.com>
2014-11-28 16:17:37 +0530

    Parallel downloading threading functionality added

--- Downloader.java
+++ Downloader.java
@@ -4,60 +4,95 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 public class Downloader {
 	private String finalURI;
 	private String destPath;
 	private HttpURLConnection con;
+	private int numSegments = 1;
+	
+	public String fileName;
+	public long fileSize;
 	
 	public Downloader(String url, String path) {
 		URIExplore uri = new URIExplore(url);
 		Logger.debug(uri.finalURI);
 		finalURI = uri.finalURI;
 		destPath = path;
-		startDownloading();
+		download();
 	}
 	
-	public void startDownloading(){
+	/* Sets the filename for a file at given URL */
+	private void setfileName(){
+		String disposition = con.getHeaderField("Content-Disposition");
+		String contentType = con.getContentType();
+
+		if(disposition != null){
+			int index = disposition.indexOf("filename=");
+			if (index > 0)
+				fileName = disposition.substring(index+10, disposition.length()-1);
+		}
+		else if(contentType.split(";")[0].equals("text/html"))
+			fileName = "index.html";
+		else
+			fileName = finalURI.substring(finalURI.lastIndexOf("/") + 1, finalURI.length());	
+	}
+	
+	/* Sets up a connection to the given finalURI */
+	private HttpURLConnection makeConnection(){
 		try{
-			/* prevent further redirects */
-			HttpURLConnection.setFollowRedirects(false);
 			/* support for proxy connections */
 			if(ConnectionProxy.proxyHTTP != null)
-				con = (HttpURLConnection) new URL(finalURI).openConnection(ConnectionProxy.proxyHTTP);
+				return (HttpURLConnection) new URL(finalURI).openConnection(ConnectionProxy.proxyHTTP);
 			else
-				con = (HttpURLConnection) new URL(finalURI).openConnection();
-			
-			con.setRequestMethod("GET");
-			//TODO: check for 206 (partial content) accept ranges header
-			
-			//con.setRequestProperty("", "");
-			int responseCode = con.getResponseCode();
-
-			Logger.debug(""+con.getHeaderFields());
-			
-			if(responseCode == HttpURLConnection.HTTP_OK){
-				String disposition = con.getHeaderField("Content-Disposition");
-				String contentType = con.getContentType();
-
-				String fileName = "";
-				if(disposition != null){
-					int index = disposition.indexOf("filename=");
-					if (index > 0)
-						fileName = disposition.substring(index+9, disposition.length());
-				}
-				else if(contentType.split(";")[0].equals("text/html"))
-					fileName = "index.html";
-				else
-					fileName = finalURI.substring(finalURI.lastIndexOf("/") + 1, finalURI.length());
+				return (HttpURLConnection) new URL(finalURI).openConnection();
+		}catch(Exception e){
+			Logger.log(Logger.Status.ERR_CONN, e.getMessage());
+			return null;
+		}
+	}
+	
+	private void download(){
+		/* prevent further redirects */
+		HttpURLConnection.setFollowRedirects(false);
+		
+		/* Make a connection and a HEAD request to determine size and name */
+		try{
+			con = makeConnection();
+			if (con != null){
+				con.setRequestMethod("HEAD");
+				Logger.debug(""+con.getHeaderFields());
 				
-				// input stream from HTTP connection is opened
+				/* set file size (in bytes) and name */
+				fileSize = con.getContentLength();			
+				int responseCode = con.getResponseCode();
+				if(responseCode == HttpURLConnection.HTTP_OK){
+					setfileName();
+					/* Download in segments if server supports Accept-Ranges header */
+					String rangeSupport = con.getHeaderField("Accept-Ranges");
+					if (rangeSupport != null)
+						numSegments = 5;
+					con.disconnect();
+					
+					startDownloading();
+				}
+			}
+		}catch(Exception e){
+			Logger.log(Logger.Status.ERR_CONN, e.getMessage());
+			return;
+		}
+	}
+	
+	private void startDownloading(){
+		/* No segment downloading, directly download the whole file */
+		if(numSegments==1){
+			try{
+				con = makeConnection();
+				con.setRequestMethod("GET");
 				InputStream inputStream = con.getInputStream();
 				String filesavePath = destPath + File.separator + fileName;
-				Logger.debug(filesavePath);
-				
+	
 				FileOutputStream outputStream = new FileOutputStream(filesavePath);
-				
+	
 				int bytesRead = -1;
 				byte[] buffer = new byte[4096];
 				int total = 0;
@@ -66,16 +101,68 @@ public class Downloader {
 					total += bytesRead;
 					Logger.debug("Wrote : "+bytesRead + " bytes");
 				}
-				
-				Logger.debug((total/1024)/1024+" MB written.");
+	
+				Logger.debug(((float)total/1024)/1024+" MB written.");
 				outputStream.close();
 				inputStream.close();
+			}catch(Exception e){
+				Logger.log(Logger.Status.ERR_CONN, e.getMessage());
+				return;
+			}finally{
+				con.disconnect();
 			}
-		}catch(Exception e){
-			Logger.log(Logger.Status.ERR_CONN, e.getMessage());
-			return;
-		}finally{
-			con.disconnect();
+		}
+		/* Make threads for each segment download */
+		else{
+			long startByte = 0;
+			long segmentSize = fileSize/numSegments + numSegments;
+			for(int i=1; i<=numSegments; i++){
+				(new downloadSegment(i, startByte, segmentSize)).start();
+				startByte += segmentSize;
+			}
+		}
+	}
+	
+	/* Class to work as thread for downloading one segment */
+	private class downloadSegment extends Thread{
+		private String segmentName;
+		private HttpURLConnection segConnection;
+		private int segNo;
+		private long startByte, segmentSize;
+		
+		downloadSegment(int segNo, long startByte, long segmentSize) {
+			segmentName = fileName + ".part" + segNo;
+			segConnection = makeConnection();
+			this.startByte = startByte;
+			this.segmentSize = segmentSize;
+			this.segNo = segNo;
+		}
+		
+		/* Thread downloads a particular range of file from server on an independent connection */
+		public void run(){
+			try{
+				segConnection.setRequestMethod("GET");
+				segConnection.setRequestProperty("Range", "bytes=" + startByte + "-" + (startByte+segmentSize-1));
+				InputStream inputStream = segConnection.getInputStream();
+				String segmentsavePath = destPath + File.separator + segmentName;
+				
+				FileOutputStream outputStream = new FileOutputStream(segmentsavePath);
+				int bytesRead = -1;
+				byte[] buffer = new byte[4096];
+				int total = 0;
+				while((bytesRead = inputStream.read(buffer)) != -1){
+					outputStream.write(buffer, 0, bytesRead);
+					total += bytesRead;
+					Logger.debug("Segment" + segNo + ": Wrote : "+bytesRead + " bytes");
+				}
+	
+				Logger.debug("Segment" + segNo + ": " + ((float)total/1024)/1024+" MB written.");
+				outputStream.close();
+				inputStream.close();
+			}catch(Exception e){
+				Logger.log(Logger.Status.ERR_CONN, e.getMessage());
+				return;
+			}
 		}
 	}
 }
