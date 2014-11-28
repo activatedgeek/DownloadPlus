c362e6f6f1e1cf6af0337a8bfefc47317fe2ddd8
activatedgeek <ksanyam18@yahoo.com>
2014-11-29 03:12:14 +0530

    lots of properties for GUI

--- DownloadUnit.java
+++ DownloadUnit.java
@@ -1,126 +1,182 @@
-
+import java.io.File;
 import java.time.LocalDateTime;
 
 import javafx.concurrent.Task;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.ProgressIndicator;
 
-public class DownloadUnit extends Task<Void>   {
+public class DownloadUnit /*extends Task<Void>*/ {
 	public enum TableField {
-		FILE_NAME,SIZE,TRANSFER_RATE,PERCENT_DOWNLOADED,FILE_DOWNLOADED,
-		STATUS
-	}
-
-    private static final int NUM_ITERATIONS = 100;
-	private String fileName = null, origin = null, url = null, folder = null;
-    private String size = null, status = null;
-    private int segments;
-    private boolean resume;
-    private LocalDateTime start, scheduled, elapsed, finish;
-    //TODO: save chunks list
-    
-    private String transferRate = null;
-
-    public DownloadUnit (String url){
-    	origin = url;
-    	fileName = url;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-    	this.fileName = fileName;
-    	//TODO: update the GUI
-    }
-    
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-		//TODO: update GUI
+		FILENAME,ORIGIN,URL,FOLDER,SIZE,STATUS,TRANSFER_RATE,
+		PROGRESS,SEGMENTS,RESUME,START,SCHEDULED,FINISH
 	}
 	
-	public String getSize() {
-		return size;
-	}
+	private long uid;
+	private SimpleStringProperty filename;
+	private SimpleStringProperty origin;
+	private SimpleStringProperty url;
+	private SimpleStringProperty folder;
+	private SimpleStringProperty size;
+	private SimpleStringProperty status;
+	private SimpleStringProperty transferRate;
+	private SimpleDoubleProperty progress;
+    private SimpleIntegerProperty segments;
+    private SimpleBooleanProperty resume;
+    private SimpleObjectProperty<LocalDateTime> start, scheduled, finish;
 
-	public void setSize(String size) {
-		this.size = size;
-		//TODO: update GUI
-	}
+    public DownloadUnit (String uri){
+    	filename = new SimpleStringProperty(uri);
+    	origin = new SimpleStringProperty(uri);
+    	url = new SimpleStringProperty(uri);
+    	folder = new SimpleStringProperty(System.getProperty("user.home")+File.separator+"/Downloads");
+    	size = new SimpleStringProperty("0 B");
+    	status = new SimpleStringProperty("Waiting");
+    	transferRate = new SimpleStringProperty("0 Bps");
+    	progress = new SimpleDoubleProperty(0);
+    	segments = new SimpleIntegerProperty(0);
+    	resume = new SimpleBooleanProperty(false);
+    	start = new SimpleObjectProperty<LocalDateTime>();
+    	scheduled = new SimpleObjectProperty<LocalDateTime>();
+    	finish = new SimpleObjectProperty<LocalDateTime>();
+    }
+    
+    public void setUID(long uid){
+    	this.uid = uid;
+    }
+    
+    public long getUID(){
+    	return uid;
+    }
 
-	public String getTransferRate() {
-		return transferRate;
-	}
+    public Object getProperty(TableField tf){
+    	switch(tf){
+    	case FILENAME:
+    		return filename.get();
+    	case ORIGIN:
+    		return origin.get();
+    	case URL:
+    		return url.get();
+    	case FOLDER:
+    		return folder.get();
+    	case SIZE:
+    		return size.get();
+    	case STATUS:
+    		return status.get();
+    	case TRANSFER_RATE:
+    		return transferRate.get();
+    	case PROGRESS:
+    		return progress.get();
+    	case SEGMENTS:
+    		return segments.get();
+    	case RESUME:
+    		return resume.get();
+    	case START:
+    		return start.get();
+    	case SCHEDULED:
+    		return scheduled.get();
+    	case FINISH:
+    		return finish.get();
+    	}
+    	return null;
+    }
 
-	public void setTransferRate(String transferRate) {
-		this.transferRate = transferRate;
-		//TODO: update GUI
-	}
-	
-	public void update(TableField field,Object value) {	
-		switch(field) {
-		case FILE_NAME:
-			this.setFileName((String)value);
-		
-		case SIZE:
-			this.setSize((String)value);
-			break;
-			
-		case FILE_DOWNLOADED:
-			this.setSize((String)value);
-			break;
-					
-		case TRANSFER_RATE:
-			this.setTransferRate((String)value);
-			break;
-		
-		case STATUS:
-			this.setStatus((String)value);
-			break;
-			
-		case PERCENT_DOWNLOADED:
-			//update the progress bar
-			this.updateProgressBar((int)value);
-			break;
-			
-		default:
-			break;
-		}
-		
-		
-	}
+    public void setProperty(TableField tf, Object value){
+    	switch(tf){
+    	case FILENAME:
+    		filename.set((String)value);
+    		break;
+    	case ORIGIN:
+    		origin.set((String)value);
+    		break;
+    	case URL:
+    		url.set((String)value);
+    		break;
+    	case FOLDER:
+    		folder.set((String)value);
+    		break;
+    	case SIZE:
+    		size.set((String)value);
+    		break;
+    	case STATUS:
+    		status.set((String)value);
+    		break;
+    	case TRANSFER_RATE:
+    		transferRate.set((String)value);
+    		break;
+    	case PROGRESS:
+    		progress.set((Double)value);
+    		break;
+    	case SEGMENTS:
+    		segments.set((Integer)value);
+    		break;
+    	case RESUME:
+    		resume.set((boolean)value);
+    		break;
+    	case START:
+    		start.set((LocalDateTime)value);
+    		break;
+    	case SCHEDULED:
+    		scheduled.set((LocalDateTime)value);
+    		break;
+    	case FINISH:
+    		finish.set((LocalDateTime)value);
+    		break;
+    	default:
+    		break;
+    	}
+    }
+    
+    public StringProperty filenameProperty(){
+    	return filename;
+    }
+    
 
-		
-	public void updateProgressBar(int percentage) {
-		
+    public StringProperty sizeProperty(){
+    	return size;
+    }
+
+    public StringProperty statusProperty(){
+    	return status;
+    }
+
+    public StringProperty transferRateProperty(){
+    	return transferRate;
+    }
+    
+    /*
+	public void updateProgressBar(double percentage) {
 		this.updateProgress(ProgressIndicator.INDETERMINATE_PROGRESS, 1);
 		
-		if(percentage==0) {
-			this.updateMessage("Waiting...");
+		if(percentage == 0) {
 			this.updateProgress(0,1);
 		}
-	    if (percentage>0 && percentage<100) {  
-	    	this.updateMessage("Running...");
-	    //Thread.sleep(2000);
-        //this.updateMessage("Running...");
-	      //  for (int i = 0; i < NUM_ITERATIONS; i++) {
-	        updateProgress((1.0 * percentage) / NUM_ITERATIONS, 1);
-	      //    Thread.sleep(400);s
+		else if (percentage > 0 && percentage < 100) {
+	        updateProgress((1.0 * percentage) / 100, 1);
         }
-	    else if (percentage==100) {
-	    	this.updateMessage("Done");
+	    else if (percentage >= 100) {
 	    	this.updateProgress(1, 1);
 	    }
 	}
 
 	@Override
 	protected Void call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	      this.updateProgress(ProgressIndicator.INDETERMINATE_PROGRESS, 1);
+	      this.updateMessage("Waiting...");
+	      Thread.sleep(1000);
+	      this.updateMessage("Running...");
+	      for (int i = 0; i < 100; i++) {
+	        updateProgress((1.0 * i) / 100, 1);
+	        Thread.sleep(100);
+	      }
+	      this.updateMessage("Done");
+	      this.updateProgress(1, 1);
+	      return null;
 	}
+	*/
     
 }
